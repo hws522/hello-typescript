2df763429f923c58c50c8b592e5bc68f235c571e
hws522 <sraphic@kakao.com>
2022-02-26 22:41:22 +0900

    type

---
+++ index.ts
@@ -0,0 +1,31 @@
+let myName: string = 'string';
+let myAge = 123;
+let myCountry = 'korea';
+let group = {
+  name: '123',
+  age: 123,
+};
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+};
+
+/*
+ *그렇다고 모든 변수에 타입지정하러 다니면 초보티가 납니다. 
+
+ *숙련자들은 타입을 귀찮게 굳이 적지 않습니다.
+
+ *왜냐면 변수 생성시 타입스크립트가 타입을 자동으로 부여해주니까요. 
+
+ 
+
+*let 이름 = 'kim';
+*let 나이 = 20;
+*이렇게만 써도 자동으로 이름변수는 string, 나이 변수는 number를 가지고 있습니다.
+
+*(변수명에 마우스 올려보면 바로바로 확인가능)
+
+*array, object 만들 때도 자동으로 알아서 됩니다. 굳이 복잡하게 타입 명시할 필요 없음  
+*/
