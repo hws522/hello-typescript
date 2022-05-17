7f296e0e21135dc8c858f844c7251ca990724469
hws522 <sraphic@kakao.com>
2022-05-17 20:53:54 +0900

    object 타입 변환기 rpactice

--- dist/index.js
+++ dist/index.js
@@ -6,20 +6,17 @@
 //   age: 123,
 // };
 let obj = {
-    model: 'k5',
-    brand: 'kia',
-    price: 6000,
-    year: 2030,
-    date: '6월',
-    percent: '5%',
-    dealer: '김차장',
+    color: '노랑',
+    model: '444',
+    price: 1234,
 };
 let obj2 = {
-    'font-size': 1,
-    'secondary': {
-        'font-size': 12,
-        'third': {
-            'font-size': 14
-        }
-    }
+    color: "",
+    model: "",
+    price: ""
+};
+let obj3 = {
+    color: [],
+    model: [],
+    price: []
 };
--- src/index.ts
+++ src/index.ts
@@ -849,57 +849,115 @@ let dog2 :Dog = { name : 'paw' }
 
 /***********************************************************************/
 
+// /**
+//  * 다음 자료의 타입을 지정해보십시오.
+//  let obj = {
+//    model: 'k5',
+//    brand: 'kia',
+//    price: 6000,
+//    year: 2030,
+//    date: '6월',
+//    percent: '5%',
+//    dealer: '김차장',
+//  }
+//  */
+
+// interface practiceType {
+//   [key: string]: string | number
+// }
+
+// let obj: practiceType = {
+//   model: 'k5',
+//   brand: 'kia',
+//   price: 6000,
+//   year: 2030,
+//   date: '6월',
+//   percent: '5%',
+//   dealer: '김차장',
+// }
+
+// /**
+//  * 다음 object 자료의 타입을 interface 써서 만들어보십시오.
+//  let obj = {
+//    'font-size' : 10,
+//    'secondary' : {
+//      'font-size' : 12, 
+//      'third' : {
+//        'font-size' : 14
+//      }
+//    }
+//  }
+//  */
+
+// interface practiceType2 {
+//   'font-size': number,
+//   [key: string]: practiceType2 | number
+// }
+
+// let obj2: practiceType2 = {
+//   'font-size': 1,
+//   'secondary': {
+//     'font-size': 12,
+//     'third': {
+//       'font-size': 14
+//     }
+//   }
+// }
+
+/***********************************************************************/
+
 /**
- * 다음 자료의 타입을 지정해보십시오.
- let obj = {
-   model: 'k5',
-   brand: 'kia',
-   price: 6000,
-   year: 2030,
-   date: '6월',
-   percent: '5%',
-   dealer: '김차장',
+ * 다음 타입을 변환기를 돌려보아라.
+ type Bus = {
+   color :string,
+   model :boolean,
+   price :number,
  }
+ * 모든 속성은 전부 string 또는 number 타입이어야 함
+ * 변환기를 만들어 기존 Bus 타입을 변환기 돌려서 조건을 충족하는
+ * 새로운 타입을 하나 만들어라.
  */
 
-interface practiceType {
-  [key: string]: string | number
+type Bus = {
+  color: string,
+  model: boolean,
+  price: number,
 }
 
-let obj: practiceType = {
-  model: 'k5',
-  brand: 'kia',
-  price: 6000,
-  year: 2030,
-  date: '6월',
-  percent: '5%',
-  dealer: '김차장',
+type TypeChanger<T> = {
+  [key in keyof T]: string | number;
+}
+
+type NewBus = TypeChanger<Bus>;
+
+let obj: NewBus = {
+  color: '노랑',
+  model: '444',
+  price: 1234,
 }
 
 /**
- * 다음 object 자료의 타입을 interface 써서 만들어보십시오.
- let obj = {
-   'font-size' : 10,
-   'secondary' : {
-     'font-size' : 12, 
-     'third' : {
-       'font-size' : 14
-     }
-   }
- }
+ * object 안에 들어있는 모든 속성을
+ * string, number 이렇게 고정된 타입으로 변환해주는게 아니라
+ * 내가 원하는 타입을 입력하면 그걸로 변환해주는
+ * 범용성 좋은 변환기를 만들어라.
  */
 
-interface practiceType2 {
-  'font-size': number,
-  [key: string]: practiceType2 | number
+type TypeChanger2<T, N> = {
+  [key in keyof T]: N;
+};
+
+type NewBus2 = TypeChanger2<Bus, string>;
+type NewBus3 = TypeChanger2<Bus, string[]>;
+
+let obj2: NewBus2 = {
+  color: "",
+  model: "",
+  price: ""
 }
 
-let obj2: practiceType2 = {
-  'font-size': 1,
-  'secondary': {
-    'font-size': 12,
-    'third': {
-      'font-size': 14
-    }
-  }
+let obj3: NewBus3 = {
+  color: [],
+  model: [],
+  price: []
 }
